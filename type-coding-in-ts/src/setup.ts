4e1a03932f7e3080fe0a3e98abab1389bc096184
Garth.Gilmour <garth.gilmour@instil.co>
2022-06-01 21:44:19 +0100

    Add iteration showing Conditional Types

--- type-coding-in-ts/src/setup.ts
+++ type-coding-in-ts/src/setup.ts
@@ -3,10 +3,12 @@ import {doSetupV1} from "./iteration-1/implementation";
 import {doSetupV2} from "./iteration-2/implementation";
 import {doSetupV3} from "./iteration-3/implementation";
 import {doSetupV4} from "./iteration-4/implementation";
+import {doSetupV5} from "./iteration-5/implementation";
 
 export function doSetup() {
     //doSetupV1();
     //doSetupV2();
     //doSetupV3();
-    doSetupV4();
+    //doSetupV4();
+    doSetupV5();
 }
