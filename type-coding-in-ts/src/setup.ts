fc8f7dc1693fa53c141f5a62261de2ac3cd34f4b
Garth.Gilmour <garth.gilmour@instil.co>
2022-06-01 15:08:52 +0100

    Add first four iterations

---
+++ type-coding-in-ts/src/setup.ts
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {doSetupV1} from "./iteration-1/implementation";
+import {doSetupV2} from "./iteration-2/implementation";
+import {doSetupV3} from "./iteration-3/implementation";
+import {doSetupV4} from "./iteration-4/implementation";
+
+export function doSetup() {
+    //doSetupV1();
+    //doSetupV2();
+    //doSetupV3();
+    doSetupV4();
+}
